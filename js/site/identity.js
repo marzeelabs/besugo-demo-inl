72ddf0f75ce92016042518ba601e7233a84393fb
Luís Miguel <luism@marzeelabs.org>
2019-09-18 10:55:58 +0100

    Test

--- js/site/identity.js
+++ js/site/identity.js
@@ -1,5 +1,11 @@
-if (!netlifyIdentity.currentUser()) {
-  netlifyIdentity.on('login', () => {
-    window.location = '/admin/';
-  });
-}
+console.log({ test1: netlifyIdentity.currentUser() });
+
+netlifyIdentity.init();
+
+console.log({ test2: netlifyIdentity.currentUser() });
+
+netlifyIdentity.on('init', (user) => { console.log({ user, test3: netlifyIdentity.currentUser() }); });
+
+netlifyIdentity.on('login', () => {
+  window.location = '/admin/';
+});
