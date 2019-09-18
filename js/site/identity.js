85a215ac6bacf0b173b632cca72ee9f832f303bb
Luís Miguel <luism@marzeelabs.org>
2019-09-18 11:00:20 +0100

    Final(?) attempt at reducing overzealous redirects

--- js/site/identity.js
+++ js/site/identity.js
@@ -1,11 +1,7 @@
-console.log({ test1: netlifyIdentity.currentUser() });
-
-netlifyIdentity.init();
-
-console.log({ test2: netlifyIdentity.currentUser() });
-
-netlifyIdentity.on('init', (user) => { console.log({ user, test3: netlifyIdentity.currentUser() }); });
-
-netlifyIdentity.on('login', () => {
-  window.location = '/admin/';
+netlifyIdentity.on('init', (user) => {
+  if (!user) {
+    netlifyIdentity.on('login', () => {
+      window.location = '/admin/';
+    });
+  }
 });
